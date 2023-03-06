8948e114ed4a3675e7ee16d2fa3f57a95ab38c5e
akashgreninja <akashuhulekal@gmail.com>
2023-03-06 22:54:57 +0530

    added first

---
+++ main/static/js/check.js
@@ -0,0 +1,25 @@
+happ_books=[
+    {
+        "title": "The Happiness Project",
+        "author": "Gretchen Rubin",
+        "genre": "Self-help"
+      },
+      {
+        "title": "The Alchemist",
+        "author": "Paulo Coelho",
+        "genre": "Fiction"
+      },
+      {
+        "title": "Eat, Pray, Love",
+        "author": "Elizabeth Gilbert",
+        "genre": "Memoir"
+      },
+      {
+        "title": "The Little Book of Hygge",
+        "author": "Meik Wiking",
+        "genre": "Lifestyle"
+      },
+];
+
+
+console.log(happ_books[1].author)
